04fc51f11e58acc95e54c371b750be82d91db5d3
longyap <long1005@live.com>
2023-02-19 22:28:20 +0800

    a

---
+++ go_module/checklocal.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"libvirt.org/go/libvirt"
+)
+
+func main() {
+	conn, err := libvirt.NewConnect("qemu:///system")
+	if err != nil {
+		log.Fatalf("failed to dial libvirt: %v", err)
+
+	}
+	defer conn.Close()
+
+	doms, err := conn.ListAllDomains(libvirt.CONNECT_LIST_DOMAINS_ACTIVE)
+	if err != nil {
+		log.Fatalf("failed to dial libvirt: %v", err)
+	}
+
+	fmt.Printf("%d running domains:\n", len(doms))
+	for _, dom := range doms {
+		name, err := dom.GetName()
+		if err == nil {
+			fmt.Printf("  %s\n", name)
+		}
+		dom.Free()
+	}
+}
