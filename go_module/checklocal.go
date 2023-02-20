e42233e1d06e71011d200bdf79a3508f14e53d35
longyap <long1005@live.com>
2023-02-20 13:09:41 +0800

    a

--- go_module/checklocal.go
+++ go_module/checklocal.go
@@ -14,15 +14,31 @@ func main() {
 
 	}
 	defer conn.Close()
-
+	//	indoms, err := conn.ListAllDomains(libvirt.CONNECT_LIST_DOMAINS_INACTIVE)
+	//	if err != nil {
+	//		log.Fatalf("failed to dial libvirt: %v", err)
+	//	}
 	doms, err := conn.ListAllDomains(libvirt.CONNECT_LIST_DOMAINS_ACTIVE)
 	if err != nil {
 		log.Fatalf("failed to dial libvirt: %v", err)
 	}
 
-	fmt.Printf("%d running domains:\n", len(doms))
+	for _, dom := range doms {
+		uuid, err := dom.GetUUIDString()
+		vcpu, err := dom.GetVcpus()
+		vcpuinfo, err := dom.GetOSType()
+		name, err := dom.GetName()
+
+		if err == nil {
+			fmt.Printf(vcpuinfo, "\n", uuid, "\n", name, "  %s\n", vcpu)
+		}
+		dom.Free()
+	}
+
+	fmt.Printf("\n%d running domains:\n", len(doms))
 	for _, dom := range doms {
 		name, err := dom.GetName()
+
 		if err == nil {
 			fmt.Printf("  %s\n", name)
 		}
