ae1bab55c11c1696e05f03202f6623d40301e986
longyap <long1005@live.com>
2023-02-24 00:51:05 +0800

    b

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -20,7 +20,7 @@
     <hr>
     
   <div class="p-4 sm:ml-12 sm:mr-12">
-   <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+   <div class="p-4">
       <div class="grid grid-cols-3 gap-4 mb-4">
          <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
             <p class="text-2xl text-gray-400 dark:text-gray-500">+</p>
