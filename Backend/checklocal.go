85ef3d2eb919cf16a42aa74fc20cf82bfc5fd122
longyap <long1005@live.com>
2023-02-28 15:40:36 +0800

    a

--- Backend/checklocal.go
+++ Backend/checklocal.go
@@ -23,7 +23,16 @@ type vm struct {
 var vmlists []vm
 
 func getvm(c *gin.Context) {
-	getlist()
+	dom := getdomain()
+	getlist(dom)
+	c.IndentedJSON(http.StatusOK, vmlists)
+	vmlists = vmlists[:0]
+}
+
+func queryvm(c *gin.Context) {
+	dom := getdomain()
+	uuid := c.Query("uuid")
+	filterlist(uuid, dom)
 	c.IndentedJSON(http.StatusOK, vmlists)
 	vmlists = vmlists[:0]
 }
@@ -49,7 +58,7 @@ func stateswitches(no libvirt.DomainState) string {
 
 }
 
-func getlist() {
+func getdomain() []libvirt.Domain {
 	conn, err := libvirt.NewConnectReadOnly("qemu:///system")
 	if err != nil {
 		log.Fatalf("failed to dial libvirt: %v", err)
@@ -64,6 +73,9 @@ func getlist() {
 	if err != nil {
 		log.Fatalf("failed to dial libvirt: %v", err)
 	}
+	return alldoms
+}
+func getlist(alldoms []libvirt.Domain) {
 	for _, dom := range alldoms {
 		uuid, err := dom.GetUUIDString()
 		info, err := dom.GetInfo()
@@ -83,11 +95,31 @@ func getlist() {
 	}
 
 }
+func filterlist(queryuuid string, alldoms []libvirt.Domain) {
+	for _, dom := range alldoms {
+		uuid, err := dom.GetUUIDString()
+		info, err := dom.GetInfo()
+		name, err := dom.GetName()
+		cmd := exec.Command("date")
+		date, err := cmd.Output()
+
+		if err != nil {
+			log.Fatalf("", err)
+
+		}
+		if queryuuid == uuid {
+			state := stateswitches(info.State)
+			vms := vm{uuid, name, state, info.NrVirtCpu, math.Round(float64(info.Memory) / 1000000), string(date)}
+			vmlists = append(vmlists, vms)
+			dom.Free()
+		}
+	}
+}
 func main() {
 	router := gin.Default()
 	router.Use(cors.Default())
 	router.GET("/api/vm", getvm)
-
+	router.GET("/api/vm/1", queryvm)
 	router.Run("localhost:8080")
 
 }
